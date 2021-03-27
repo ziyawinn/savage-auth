f21350c6efe6332dbfe3aaaa3f7976a528faab6e
ziyawinn <ziyawinn@gmail.com>
2021-03-27 17:45:43 -0400

    first commit

--- app/routes.js
+++ app/routes.js
@@ -34,11 +34,25 @@ module.exports = function(app, passport, db) {
       })
     })
 
-    app.put('/messages', (req, res) => {
+    app.put('/thumpUp', (req, res) => {
       db.collection('messages')
       .findOneAndUpdate({name: req.body.name, msg: req.body.msg}, {
         $set: {
           thumbUp:req.body.thumbUp + 1
+        }
+      }, {
+        sort: {_id: -1},
+        upsert: true
+      }, (err, result) => {
+        if (err) return res.send(err)
+        res.send(result)
+      })
+    })
+    app.put('/thumbDown', (req, res) => {
+      db.collection('messages')
+      .findOneAndUpdate({name: req.body.name, msg: req.body.msg}, {
+        $set: {
+          thumbDown:req.body.thumbDown + 1
         }
       }, {
         sort: {_id: -1},
